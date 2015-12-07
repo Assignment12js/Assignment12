66708e74e3e03450d8d3cc596a6f67b3432a2e6e
Sam Gearou <samgearou@gmail.com>
2015-12-06 20:51:15 -0800

    more testing

--- src/Dijkstra.c
+++ src/Dijkstra.c
@@ -53,24 +53,34 @@ bool isStronglyConnected(graph* g){
     }
   }
 }
-void Dijkstra(graph* g) {
+void Dijkstra(graph* g, int index, char type) {
   double priority;
-  vertex_t start =readFile().trips[0].start; //start of the trip
+  vertex_t start =readFile().trips[index].start; //start of the trip
   size_t n=graph_vertex_count(g); //number of vertices in the graph
   size_t roadC=graph_edge_count(g);
   int s= (int) roadC;
   vertex_t cur;
   double length[n][n]; //distance of a road in miles
+  double speed[n][n]; //speed of the vehicle in miles per hour
   double dist[n]; // current best distance
   vertex_t parent[n]; //array of parent pointers
   dist[start]=0; //start distance
   pqueue pq; //priority queue
   pqueue_init(&pq); //initialization of the priority queue
   pqueue_push(&pq, start, 0); //push the start into pqueue
-  for (int i=0; i<s; i++) {
-    length[ readFile().roads[i].start][ readFile().roads[i].end]=readFile().roads[i].distance;
+  for (int i=0; i<s; i++){
+  if(type== 'D'){
+    length[readFile().roads[i].start][readFile().roads[i].end]=readFile().roads[i].distance;
   }
-  //printf("this is it: %f\n", length[0][1]);
+  else if(type == 'T'){
+length[readFile().roads[i].start][readFile().roads[i].end]=readFile().roads[i].distance/readFile().roads[i].speed;
+      //this will all be in terms of hours!! need to change for the output
+  }
+}
+  for(int i=0; i<s; i++){
+     speed[readFile().roads[i].start][readFile().roads[i].end]=readFile().roads[i].speed;
+  }
+  //printf("%s\n", readFile().locations[0].name); //LOOK
   size_t deg[1];
   vertex_t neighbors[n];
   vertex_t vkey;
@@ -101,13 +111,18 @@ if (d<=dist[cur]) {
   }
 }
 }
-printf("this is it: %f\n", dist[6]);
+printf("%s\n", readFile().locations[14].name);
+if(type == 'D'){
+
+}
 }
 
 int main(){
   file_record fr =readFile();
   graph* obj = buildGraph();
-  Dijkstra(obj);
+  int v = 0;
+  char ok = 'D';
+  Dijkstra(obj, v, ok);
 
 
   return EXIT_SUCCESS;
